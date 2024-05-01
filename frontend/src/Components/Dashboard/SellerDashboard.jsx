ba647cfa03c328283d5b4f1431860b5bb0e14b0b
Ali Shahal <alishahal9656@gmail.com>
2024-05-01 17:14:58 +0530

    commit initial files

---
+++ frontend/src/Components/Dashboard/SellerDashboard.jsx
@@ -0,0 +1,175 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
+
+const EditProductModal = ({ isOpen, onClose, product }) => {
+  const [editedProduct, setEditedProduct] = useState({ ...product });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setEditedProduct({ ...product });
+  }, [product]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProduct(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const token = localStorage.getItem('token');
+      await axios.put(`${import.meta.env.VITE_REACT_APP_API_ENDPOINT}/products/${editedProduct._id}`, editedProduct, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      alert('Product updated successfully');
+      onClose();
+    } catch (error) {
+      console.error('Error updating product:', error);
+      alert('Failed to update product');
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className={`fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50 ${isOpen ? 'visible' : 'invisible'}`}>
+      <div className="bg-white p-8 rounded-lg">
+        <h2 className="text-2xl font-semibold mb-4">Edit Product</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700">Name:</label>
+            <input type="text" id="name" name="name" value={editedProduct.name} onChange={handleChange} className="w-full border border-gray-300 rounded-md px-4 py-2" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block text-gray-700">Description:</label>
+            <input type="text" id="description" name="description" value={editedProduct.description} onChange={handleChange} className="w-full border border-gray-300 rounded-md px-4 py-2" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="location" className="block text-gray-700">Location:</label>
+            <input type="text" id="location" name="location" value={editedProduct.location} onChange={handleChange} className="w-full border border-gray-300 rounded-md px-4 py-2" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="category" className="block text-gray-700">Category:</label>
+            <input type="text" id="category" name="category" value={editedProduct.category} onChange={handleChange} className="w-full border border-gray-300 rounded-md px-4 py-2" />
+          </div>
+          <div className="flex justify-end">
+            <button type="button" onClick={onClose} className="mr-2 bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600">Cancel</button>
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" disabled={loading}>Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const SellerDashboard = () => {
+  const [myProducts, setMyProducts] = useState([]);
+  const [activeMenu, setActiveMenu] = useState('myProducts');
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const userId = jwtDecode(localStorage.getItem("token")).userId;
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    axios.get(`${import.meta.env.VITE_REACT_APP_API_ENDPOINT}/get-products`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(res => {
+      const userProducts = res.data.filter(product => product.createdBy === userId);
+      setMyProducts(userProducts);
+    })
+    .catch(err => console.error(err));
+  }, []);
+
+  const handleDelete = async (productId) => {
+    try {
+      const token = localStorage.getItem('token');
+      await axios.delete(`${import.meta.env.VITE_REACT_APP_API_ENDPOINT}/products/remove/${productId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setMyProducts(prevProducts => prevProducts.filter(product => product._id !== productId));
+      alert('Product deleted successfully');
+    } catch (error) {
+      console.error('Error deleting product:', error);
+      alert('Failed to delete product');
+    }
+  };
+
+  const handleEdit = (product) => {
+    setSelectedProduct(product);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedProduct(null);
+  };
+
+  return (
+    <div className="flex h-screen bg-gray-900">
+      <div className="w-1/4 h-full bg-gray-800 p-8 overflow-y-auto">
+        <h1 className="text-white text-xl mb-4">Dashboard</h1>
+        
+        <ul className="text-white">
+          <li className={`mb-2 cursor-pointer hover:text-gray-300 ${activeMenu === 'myProducts' && 'text-gray-300'}`} onClick={() => setActiveMenu('myProducts')}>My Products</li>
+        </ul>
+      </div>
+      <div className="w-3/4 h-full p-8 overflow-y-auto">
+        {activeMenu === 'myProducts' && (
+          <>
+           <div className='flex justify-between mb-3'>
+
+<h1 className="text-white text-2xl mb-4">Products</h1>
+<div className="flex justify-end">
+<Link to="/add-product" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+Add Product
+</Link>
+</div>
+</div>
+            <div className="overflow-x-auto">
+              <table className="w-full table-auto bg-gray-700 text-white rounded">
+                <thead>
+                  <tr>
+                    <th className="border border-white px-4 py-2">Name</th>
+                    <th className="border border-white px-4 py-2">Product ID</th>
+                    <th className="border border-white px-4 py-2">Created At</th>
+                    <th className="border border-white px-4 py-2">Verified</th>
+                    <th className="border border-white px-4 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {myProducts.map(product => (
+                    <tr key={product._id}>
+                      <td className="border border-white px-4 py-2">{product.name}</td>
+                      <td className="border border-white px-4 py-2">{product._id}</td>
+                      <td className="border border-white px-4 py-2">{product.createdAt}</td>
+                      <td className="border border-white px-4 py-2">{product.isApproved ? 'Yes' : 'No'}</td>
+                      <td className="border border-white px-4 py-2">
+                        <button onClick={() => handleEdit(product)} className="bg-blue-500 text-white font-semibold w-20 h-8 mr-2">Edit</button>
+                        <button onClick={() => handleDelete(product._id)} className="bg-red-500 text-white font-semibold w-20 h-8">Delete</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
+      </div>
+      <EditProductModal isOpen={isModalOpen} onClose={closeModal} product={selectedProduct} />
+    </div>
+  );
+};
+
+export default SellerDashboard;
---
+++ frontend/src/Components/Products.jsx
@@ -0,0 +1,189 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ProductPage = ({ location }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedPrice, setSelectedPrice] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedFeatures, setSelectedFeatures] = useState({});
+  const [allCategories, setAllCategories] = useState([]);
+  const [allCities, setAllCities] = useState([]);
+  const [allPriceRanges, setAllPriceRanges] = useState([]);
+  const query = new URLSearchParams(location.search).get("query");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let endpoint = `${import.meta.env.VITE_REACT_APP_API_ENDPOINT}/get-products`;
+
+        if (query) {
+          endpoint = `${import.meta.env.VITE_REACT_APP_API_ENDPOINT}/products/search?query=${query}`;
+        }
+
+        const response = await axios.get(endpoint);
+        const filteredData = response.data.filter((product) => product.isApproved);
+        setProducts(filteredData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchData();
+  }, [query]);
+
+  useEffect(() => {
+    const categories = [...new Set(products.map(product => product.category))];
+    setAllCategories(categories);
+  }, [products]);
+
+  useEffect(() => {
+    const cities = [...new Set(products.map(product => product.location))];
+    setAllCities(cities);
+  }, [products]);
+
+  useEffect(() => {
+    // Define price range options based on your requirement
+    const priceRanges = [
+      { label: '$0 - $1000', value: '0-1000' },
+      { label: '$1001 - $5000', value: '1001-5000' },
+      { label: '$5001 - $10000', value: '5001-10000' },
+      // Add more price range options based on your requirement
+    ];
+    setAllPriceRanges(priceRanges);
+  }, []);
+
+  useEffect(() => {
+    applyFilters();
+  }, [selectedCategory, selectedPrice, selectedCity, selectedFeatures, products]);
+
+  const applyFilters = () => {
+    let filteredProducts = [...products];
+  
+    // Filter by category
+    if (selectedCategory) {
+      filteredProducts = filteredProducts.filter(product => product.category === selectedCategory);
+    }
+  
+    // Filter by price range
+    if (selectedPrice) {
+      const [minPrice, maxPrice] = selectedPrice.split('-').map(Number);
+      filteredProducts = filteredProducts.filter(product => {
+        const productPrice = Number(product.Price);
+        return productPrice >= minPrice && productPrice <= maxPrice;
+      });
+    }
+  
+    // Filter by city
+    if (selectedCity) {
+      filteredProducts = filteredProducts.filter(product => product.location === selectedCity);
+    }
+  
+    // Filter by selected features
+    if (Object.keys(selectedFeatures).length > 0) {
+      filteredProducts = filteredProducts.filter(product => {
+        for (const [feature, value] of Object.entries(selectedFeatures)) {
+          if (product.features[feature] !== value) {
+            return false;
+          }
+        }
+        return true;
+      });
+    }
+  
+    setFilteredProducts(filteredProducts);
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Navbar />
+      <div className="flex mt-[200px] justify-between items-center mb-4">
+        <div>
+          <select
+            className="border border-gray-300 rounded-md p-2 mr-2"
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option value="">Select Category</option>
+            {allCategories.map((category, index) => (
+              <option key={index} value={category}>{category}</option>
+            ))}
+          </select>
+          <select
+            className="border border-gray-300 rounded-md p-2 mr-2"
+            value={selectedPrice}
+            onChange={(e) => setSelectedPrice(e.target.value)}
+          >
+            <option value="">Select Price</option>
+            {allPriceRanges.map((range, index) => (
+              <option key={index} value={range.value}>{range.label}</option>
+            ))}
+          </select>
+          <select
+            className="border border-gray-300 rounded-md p-2 mr-2"
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.target.value)}
+          >
+            <option value="">Select City</option>
+            {allCities.map((city, index) => (
+              <option key={index} value={city}>{city}</option>
+            ))}
+          </select>
+          {/* Add additional filter options based on the selected category */}
+        </div>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Apply Filters</button>
+      </div>
+      <div>
+        {filteredProducts.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {filteredProducts.map((product) => (
+              <div key={product._id} className="bg-white shadow-md rounded-lg p-4">
+                <Slider {...settings}>
+                  {product.images.map((image, index) => (
+                     <img
+                     key={index}
+                     src={`${import.meta.env.VITE_REACT_APP_IMAGE_ENDPOINT}/${image}`}
+                     alt={product.name}
+                     className="object-cover"
+                   />
+                  ))}
+                </Slider>
+                <h2 className="text-lg font-semibold">{product.name}</h2>
+                <p className="text-sm text-gray-500">{product.description}</p>
+                <p className="text-lg font-bold mt-2">${product.Price}</p>
+                <div>
+                  <h3 className="text-base font-medium text-gray-900">Features:</h3>
+                  <ul className="text-sm text-gray-500">
+                    {Object.entries(product.features).map(([key, value]) => (
+                      <li key={key}>
+                        <span className="font-semibold">{key}: </span>
+                        {value}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
